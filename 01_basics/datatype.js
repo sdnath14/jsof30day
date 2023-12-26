8767a8e9773b633795b73cc966eb04735c53c0bb
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2023-12-26 14:37:45 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+//alert(3+3) we using node js not browser
+console.log("sneha");
+let name="smegha"
+let age=28
+let isfollow=false
+let state;//undefined
+console.log(typeof "smegha")
+console.log(typeof state)

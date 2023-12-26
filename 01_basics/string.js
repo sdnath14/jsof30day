8ae43cc5180f31fe284b4896ddc7d41716691498
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2023-12-26 17:04:06 +0000

    string.js

---
+++ 01_basics/string.js
@@ -0,0 +1,17 @@
+const name="s.dnath"
+const myrepocount=50
+console.log(`hello my name is ${name}and my repo count is${myrepocount}`);
+const gamename= new String('snehadeb');
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('h'));
+
+
+
+
+
+
+
+

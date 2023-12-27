c88110186e3700c3a26d8e031bba32bfc186ef88
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2023-12-27 06:39:20 +0000

    numberandmath.js

---
+++ 01_basics/numberandmath.js
@@ -0,0 +1,25 @@
+const score=400;
+const balance= new Number(100)
+console.log(balance)
+//converting number to string
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+//uses of precision
+const mynumber=23.99635
+console.log(mynumber.toPrecision(2))
+//uses of local string
+const newnum=20000000
+console.log(newnum.toLocaleString('en-IN'));
+//++++++++++++++++++++++++++++math++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.88));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.88));
+
+
+
+
+
+

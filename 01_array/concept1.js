77be4bd9131ef6d2760624ee61eca2faca48d136
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2023-12-29 18:24:32 +0000

    manipulate.js

--- 01_array/concept1.js
+++ 01_array/concept1.js
@@ -5,18 +5,22 @@
 //console.log("the length is:",myarr.length)
 /* +++++++++++++ i want to contain differnt datatypes in an array++++++++++++++++++++
  */
-const information=[{Name:'sneha Debnath'},
-{country:'india'},{city:'kolkata'},
-{Skills:['HTML','CSS','Javascipt']},
-{age:19},{sex:'female'}, 47
-]
-console.log('All about me:',information);
-console.log(typeof(information[1]));
-// let arr = new Array
-let arr = [12,3,3,432,4432,5,34,534,6,45,2,3,3]
-console.log(arr.length);
+// const information=[{Name:'sneha Debnath'},
+// {country:'india'},{city:'kolkata'},
+// {Skills:['HTML','CSS','Javascipt']},
+// {age:19},{sex:'female'}, 47
+// ]
+// console.log('All about me:',information);
+// console.log(typeof(information[1]));
+// // spiliting an array
+// let js= 'javascript'
+// const totalchar= js.split('');
+// console.log(totalchar);
+// const num=[4,45,6,8,9,20,50];
+// let arr= num.length-1;
+// console.log(num[arr]);// finding the last index of the array
+//++++++++++++++++++++modify array eliment+++++++++++++++++++++++++++++++++++++++++++++++
 
-let arr2 = arr.map(e => e*e)
-console.log(arr2);
+
 
 
---
+++ 01_array/manipulate.js
@@ -0,0 +1,9 @@
+//creating an empty array
+const arr= Array()
+let emptyarray=Array(4).fill(7);
+console.log(emptyarray);
+//concat an array
+let first=[1,2,3];
+const second=[4,5,6];
+let myfinalarr=first.concat(second);
+console.log(myfinalarr);

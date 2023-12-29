5b982400d88181b0dfc5f79830c303fb8d0d9c8a
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2023-12-29 08:33:00 +0000

    array.js

---
+++ 01_array/basic.js
@@ -0,0 +1,26 @@
+//Array
+const myarr=[1,2,3,4,5,true,"sneha"]
+// console.log(myarr[0]);
+// const newarr= new Array(1,4,6,7,8)
+// console.log(newarr[3]);
+// some array methodssarr.push(6)
+const createarr=[1,2,3,4,5,6]
+// createarr.push(7)// pushing means add new value
+// console.log(createarr);
+// // createarr.pop()//to remove the last eliment
+// // console.log(createarr);
+// createarr.shift()
+// createarr.unshift(9)
+// console.log(createarr)
+//console.log(myarr);
+// const newarr=myarr.join()
+// console.log(newarr);//converting element into a string element
+//The concept of slice and splice)
+console.log("A",myarr);
+const ma1=myarr.slice(0,3);
+console.log(ma1);
+console.log("B",myarr);
+console.log("C",myarr);
+const ma2=myarr.splice(0,3);
+console.log(ma2);
+

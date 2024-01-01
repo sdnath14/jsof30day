ce7e3418ad3497e66a67aaa5e1eede532dedc395
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2024-01-01 08:35:45 +0000

    sliceandsplice.js

---
+++ 01_array/sliceandsplice.js
@@ -0,0 +1,14 @@
+// concept of slice
+let num=[1,4,7,8,9]
+let number= num.slice(1,3);// its remove the ending position from start index also for 0
+let nums=num.slice();// it  copies the element
+let nume=num.slice(0,5)//it copies the element//numbers.length
+let numbers=num.slice(0);// it copies the element
+console.log(number);
+console.log(numbers);
+console.log(nume);
+console.log(nums);
+//++++++++++concept of slice+++++++++++++++++
+
+
+

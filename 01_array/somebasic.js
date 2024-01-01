77df7db6e8dbeade9652ec9478765d6387f0e787
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2024-01-01 06:35:37 +0000

    somebasic.js

---
+++ 01_array/somebasic.js
@@ -0,0 +1,11 @@
+//finding the last element of an array
+const num=[2,5,8,3,2];
+console.log(num.lastIndexOf(2));
+// finding an item in array is present or not
+console.log(num.includes(5));// if it is present it will return true;
+console.log(num.includes(10));//if it is not present then it will return false
+//finding if the dtatype is an array
+let number=10;
+console.log(Array.isArray(num))//returns true it is
+console.log(Array.isArray(number));//returns false if it is not
+

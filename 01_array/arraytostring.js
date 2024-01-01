716da59930cf5c48e24ed7c5d7ff86e63362dd1c
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2024-01-01 07:39:12 +0000

    arraytostring.js

---
+++ 01_array/arraytostring.js
@@ -0,0 +1,18 @@
+// converting an array into string
+const num=[1,3,5,67,8];
+let number=num.toString();
+// console.log(num.toString());//1,3,5,67,8
+// console.log(typeof number);//string
+// console.log(num);//[1,3,5,67,8]
+// console.log(typeof num);//object
+// joining array elements
+const letters=['A','B','C','D']
+console.log(letters.join());
+console.log(letters.join('3'));
+console.log(letters.join(','));
+console.log(letters.join('#'));
+console.log(letters.join(' '));
+console.log(letters.join(''));
+
+
+

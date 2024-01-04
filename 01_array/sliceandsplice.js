bcba071d21759d98416c6e71a93078746833ad28
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2024-01-04 15:52:12 +0000

    lllll

--- 01_array/sliceandsplice.js
+++ 01_array/sliceandsplice.js
@@ -4,11 +4,17 @@ let number= num.slice(1,3);// its remove the ending position from start index al
 let nums=num.slice();// it  copies the element
 let nume=num.slice(0,5)//it copies the element//numbers.length
 let numbers=num.slice(0);// it copies the element
-console.log(number);
-console.log(numbers);
-console.log(nume);
-console.log(nums);
-//++++++++++concept of slice+++++++++++++++++
-
+// console.log(number);
+// console.log(numbers);
+// console.log(nume);
+// console.log(nums);
+//++++++++++concept of splice+++++++++++++++++
+// it works in three parameters(.starting position,numberofitemsto be removed,or items added)
+let fruits=['banana','orange','apple','guava','avacado']
+fruits.splice(2,2,men,women);
+// console.log(fruits.splice());// it will remove all the elements
+// console.log(fruits.splice(0,3))// it will not remove the last eliment like slice
+// console.log(fruits.splice(0,3))
+console.log(2,2,men,women)
 
 

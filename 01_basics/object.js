75e213db50266a9739278d0accb87a0f99d180d2
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2024-01-04 16:44:58 +0000

    object.js

---
+++ 01_basics/object.js
@@ -0,0 +1,20 @@
+//objet literals
+// const jsuser={
+//     name:"sneha",
+//     age:19,
+//     location:"kolkata"
+// }
+// const obj1={1:"a",2:"b"}
+// const obj2={1:"a",2:"b"}
+// const obj3= Object.assign({},obj1,obj2);
+// console.log(obj3);
+const course=
+{
+    coursename:"javascript",
+    price:"999",
+    courseinstructor:"sneha",
+
+}
+const{courseinstructor:instructor}=course
+console.log(instructor)
+
